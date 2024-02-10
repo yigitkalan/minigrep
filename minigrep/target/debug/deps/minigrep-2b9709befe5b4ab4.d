7d69c62ceae404ac49e8c2f4c4dbcb628b4a4536
yigitkalan <sametykalan@gmail.com>
2024-02-10 22:46:51 +0300

    feat: create config to parse arguments

---
+++ minigrep/target/debug/deps/minigrep-2b9709befe5b4ab4.d
@@ -0,0 +1,5 @@
+/home/sy/work/rust/minigrep/minigrep/target/debug/deps/minigrep-2b9709befe5b4ab4: src/main.rs
+
+/home/sy/work/rust/minigrep/minigrep/target/debug/deps/minigrep-2b9709befe5b4ab4.d: src/main.rs
+
+src/main.rs:
---
+++ minigrep/target/debug/deps/minigrep-a064f375134067aa.d
@@ -0,0 +1,5 @@
+/home/sy/work/rust/minigrep/minigrep/target/debug/deps/minigrep-a064f375134067aa.rmeta: src/main.rs
+
+/home/sy/work/rust/minigrep/minigrep/target/debug/deps/minigrep-a064f375134067aa.d: src/main.rs
+
+src/main.rs:
